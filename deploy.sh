43ef0ee0c361672c17e5a38f9f642f6d11a55df5
Dimitri <dimitri@localhost.localdomain>
2018-05-14 01:30:35 -0400

    deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+#Import rpm for visual studio code
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+#Install base applications
+sudo dnf check-update
+sudo dnf install -y htop terminator vim code ansible git
+#Update everything
+sudo dnf update -y
+
