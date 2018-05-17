9fc9cd0724b3b7c4f2a8b6dd34222a30627ec1c4
Dimitri2g <34316648+Dimitri2g@users.noreply.github.com>
2018-05-17 00:49:46 -0400

    enable epel
    epel-release

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,7 @@ sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 #Install base applications
 sudo dnf check-update
+sudo dnf install -y epel-release
 sudo dnf install -y htop terminator vim code ansible git
 #Update everything
 sudo dnf update -y
